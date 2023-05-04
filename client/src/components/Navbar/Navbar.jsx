90f22278d68029869088d5baced23ea102e3f1b9
Jishnu M M <jishnumm4312@gmail.com>
2023-05-04 15:25:48 +0530

    navbar added

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+import "./Navbar.scss"
+
+const Navbar = () => {
+  //const [open,setOpen] = useState(false)
+  //const products = useSelector((state) => state.cart.products);
+
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/img/artlogo.png" height="100px" width="200px" alt="" />
+          </div>
+         
+        </div>
+        
+        <div className="right">
+          <div className="item">
+            <Link className ="link" to="/">Homepage</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/">Contact</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/">Catalogue</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon/>
+            <PersonOutlineOutlinedIcon/>
+            <FavoriteBorderOutlinedIcon/>
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon/>
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
