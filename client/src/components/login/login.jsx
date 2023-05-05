d4ff47ea097413a467aea8efa3a1151a17865ec1
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-05-05 14:10:45 +0530

    fixed login form

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,15 +1,26 @@
 import { createClient } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
+import './login.scss'
 
 const supabase = createClient('https://ulobuwxqwbruszgeonsk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsb2J1d3hxd2JydXN6Z2VvbnNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMzEzMDEsImV4cCI6MTk5ODgwNzMwMX0.qRgmFEWxzrDgjB5pjYi8MfmbaAcRvJCI-f5ZPA3YYJE')
 
 const LoginPage = () => (
-  <Auth
+  <div className='login-container'>
+    <Auth
     supabaseClient={supabase}
-    appearance={{ theme: ThemeSupa }}
-    providers={['google', 'facebook', 'twitter']}
+    appearance={{
+      theme: 'dark',
+      variables: {
+        default: {
+          colors: {
+            brand: 'olivegreen',
+            brandAccent: 'darkgreen',
+          },
+        },
+      },
+    }}
   />
+  </div>
 )
-
 export default LoginPage;
