5e5f6ba7650ff4dffff9d0d168732ae4023aae2f
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-05-05 08:45:46 +0530

    Auth Ui

--- client/src/components/login/login.jsx
+++ client/src/components/login/login.jsx
@@ -1,66 +1,15 @@
-import React, { useState } from "react";
-import "./login.scss";
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
 
+const supabase = createClient('https://ulobuwxqwbruszgeonsk.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVsb2J1d3hxd2JydXN6Z2VvbnNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyMzEzMDEsImV4cCI6MTk5ODgwNzMwMX0.qRgmFEWxzrDgjB5pjYi8MfmbaAcRvJCI-f5ZPA3YYJE')
 
-function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  return (
-    <div className="login-page">
-      {/* image section */}
-      <div className="image-section">
-        <img src="https://ajirakhabar.com/wp-content/uploads/2018/03/tribal-products.jpg" alt="Login page" />
-      </div>
-
-      {/* sign-up and login form section */}
-      <div className="form-section">
-        <h1>Sign up or Log in</h1>
-        <form>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={email}
-            onChange={handleEmailChange}
-          />
-
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={password}
-            onChange={handlePasswordChange}
-          />
-
-          <button type="submit">Sign up or Log in</button>
-        </form>
-        <br></br>
-        {/* Google sign-in button */}
-        <div className="google-btn">
-          
-          <div className="google-icon-wrapper">
-          <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
-          </div>
-          <button>
-          <p className="btn-text">
-          Sign in with Google
-            </p>
-        </button>
-        </div>
-      </div>
-    </div>
-  );
-}
+const LoginPage = () => (
+  <Auth
+    supabaseClient={supabase}
+    appearance={{ theme: ThemeSupa }}
+    providers={['google', 'facebook', 'twitter']}
+  />
+)
 
 export default LoginPage;
